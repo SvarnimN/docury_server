b31cef6bdf592d52b8e205f0e0b98bf309e3714a
Swornim Nakarmi <svarnim.nakahmi1.618@gmail.com>
2025-12-08 22:33:05 +0545

    feat: add server runner

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import file
+from app.routers import chat
+
+app = FastAPI()
+app.include_router(file.router)
+app.include_router(chat.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def index():
+    return "Welcome to you Docury."
