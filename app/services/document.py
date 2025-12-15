76e4f296de4da7926f3c53fe2c6d125599687502
Swornim Nakarmi <svarnim.nakahmi1.618@gmail.com>
2025-12-15 09:49:33 +0545

    fix: improve document metadata retrieval

--- app/services/document.py
+++ app/services/document.py
@@ -1,39 +1,57 @@
 import os
-from langchain_community.document_loaders import PyPDFLoader
+from typing import List
+from fastapi import UploadFile
+from langchain_docling import DoclingLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 from app.config import Configs
+
 
 class DocumentService:
     def __init__(
-            self, upload_dir=Configs.UPLOAD_DIR,
-            chunk_size=Configs.CHUNK_SIZE,
-            chunk_overlap=Configs.CHUNK_OVERLAP
-        ):
+        self,
+        upload_dir=Configs.UPLOAD_DIR,
+        chunk_size=Configs.CHUNK_SIZE,
+        chunk_overlap=Configs.CHUNK_OVERLAP,
+    ):
         self.upload_dir = upload_dir
         self.splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap
+            chunk_overlap=chunk_overlap,
         )
 
-    def save_file(self, upload_file) -> str:
-        os.makedirs(Configs.UPLOAD_DIR, exist_ok=True)
+    async def save_file(self, upload_file: UploadFile) -> str:
+        os.makedirs(self.upload_dir, exist_ok=True)
 
         path = os.path.join(self.upload_dir, upload_file.filename)
-        file_bytes = upload_file.file.read()
 
         with open(path, "wb") as f:
-            f.write(file_bytes)
-        
+            f.write(await upload_file.read())
+
         return path
 
-    def load_and_split(self, filepath: str):
-        loader = PyPDFLoader(filepath)
-        doc = loader.load()
-        chunks = self.splitter.split_documents(doc)
-        
-        for d_index, chunk in enumerate(chunks):
-            if "source" not in chunk.metadata:
-                chunk.metadata["source"] = filepath
-            chunk.metadata["chunk_index"] = d_index
+    def load_and_split(self, filepath: str) -> List[Document]:
+        loader = DoclingLoader(
+            file_path=filepath
+        )
+
+        docs = loader.load()
+        chunks = self.splitter.split_documents(docs)
+
+        for idx, chunk in enumerate(chunks):
+            chunk.metadata["source"] = filepath
+            chunk.metadata["chunk_index"] = idx
+
+            page_numbers = set()
+
+            dl_meta = getattr(chunk, "dl_meta", None)
+            if dl_meta:
+                for doc_item in dl_meta.get("doc_items", []):
+                    for prov in doc_item.get("prov", []):
+                        page_no = prov.get("page_no")
+                        if page_no is not None:
+                            page_numbers.add(page_no)
+
+            chunk.metadata["page_numbers"] = sorted(page_numbers) if page_numbers else None
 
         return chunks
